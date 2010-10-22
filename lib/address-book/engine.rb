a44a4adfa10ff8d4df61c4eae20901470150d7b3
Jeremiah Heller <jeremiah@inertialbit.net>
2010-10-22 08:33:18 -0700

    disable gem declaration in Engine

--- lib/address-book/engine.rb
+++ lib/address-book/engine.rb
@@ -3,11 +3,11 @@ module AddressBook
     engine_name :address_book
     config.asset_path = "/address_book%s"
     config.action_view.javascript_expansions[:address_book] = %w(jquery-1.4.1.min.js rails.js jquery.tablesorter.min.js jquery-ui-1.7.2.custom.min.js jquery.string.1.0-min.js jquery.clonePosition.js lowpro.jquery.js jquery.qtip-1.0.0-rc3.js behaviors.js)
-    config.gem 'formtastic'
-    config.gem 'will_paginate', '~> 3.0.pre2', {
-      :source => 'http://gemcutter.org'
-    }
-    config.gem 'acts_as_revisable', '>= 1.1.2'
+    #config.gem 'formtastic'
+    #config.gem 'will_paginate', '~> 3.0.pre2', {
+    #  :source => 'http://gemcutter.org'
+    #}
+    #config.gem 'acts_as_revisable', '>= 1.1.2'
   end
 
   mattr_reader :app_type
